ff8cb7b0b7c30d8d6e352053d87e6e482a9e216a
minjongbaek <minjongbaek@gmail.com>
2023-06-09 17:49:46 +0900

    feat: RecoilRootWrapper 컴포넌트 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import wallpaperSrc from "@public/images/wallpaper/ventura.jpg";
 import MenuBar from "@/components/MenuBar/MenuBar";
 import Dock from "@/components/Dock/Dock";
+import RecoilRootWrapper from "@/components/Base/RecoilRootWrapper";
 
 export const metadata = {
   title: "Desktop.minjong",
@@ -17,21 +18,23 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="w-full h-screen fixed">
-        <div className="w-full h-full absolute">
-          <Image
-            src={wallpaperSrc}
-            alt="desktop"
-            fill
-            style={{ objectFit: "cover" }}
-            quality={100}
-            placeholder="blur"
-          />
-        </div>
-        <div className="relative w-full h-full flex flex-col">
-          <MenuBar />
-          {children}
-          <Dock />
-        </div>
+        <RecoilRootWrapper>
+          <div className="w-full h-full absolute">
+            <Image
+              src={wallpaperSrc}
+              alt="desktop"
+              fill
+              style={{ objectFit: "cover" }}
+              quality={100}
+              placeholder="blur"
+            />
+          </div>
+          <div className="relative w-full h-full flex flex-col">
+            <MenuBar />
+            {children}
+            <Dock />
+          </div>
+        </RecoilRootWrapper>
       </body>
     </html>
   );
